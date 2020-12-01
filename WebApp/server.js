1f5cd35d06c13c7f6e001823858cbd9da35eb80d
madhuraKusre <75204126+madhuraKusre@users.noreply.github.com>
2020-12-02 00:46:31 +0530

    client-server

---
+++ WebApp/server.js
@@ -0,0 +1,21 @@
+                                                                    
+console.log("Welcome to server app");
+
+var http=require("http");
+
+var url=require("url");
+
+var fs=require("fs"); var onCreateServer=function(request, response) {
+
+var pathname = url.parse(request.url).pathname;
+
+fs.readFile(pathname.substr(1), function (err, data) { if (err) { console.log(err); response.writeHead(404, {'Content-Type': 'text/html'});
+}
+else { response.writeHead(200, {'Content-Type': 'text/html'}); response.write(data.toString());
+}
+response.end();
+});
+};
+var server=http.createServer(onCreateServer);
+server.listen(7000); 
+console.log("server  is listening on port no http://localhost:7000/");
